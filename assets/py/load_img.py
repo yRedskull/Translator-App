0dd9e54b76aa1a148914d3318baf211989326448
T. Felipe <eusouored@hotmail.com>
2023-06-28 23:13:36 -0300

    Update load_img.py

--- assets/py/load_img.py
+++ assets/py/load_img.py
@@ -8,5 +8,3 @@ class ImagesLoad:
         self.path_imgs = path.join(path.dirname(path.dirname(__file__)), "images")
         self.logo_bitmap = path.join(self.path_imgs, "translator.ico")
         self.logo_png = CTkImage(Image.open(path.join(self.path_imgs, "translator.png")), size=(64, 64))
-        self.equal_light_png = CTkImage(Image.open(path.join(self.path_imgs, "equal-light.png")), size=(48, 48))
-        self.equal_dark_png = CTkImage(Image.open(path.join(self.path_imgs, "equal-dark.png")), size=(48, 48))
