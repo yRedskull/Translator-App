35f2e9a9f4cbd51e65ae93ffb95230b99ce8bd01
T. Felipe <eusouored@hotmail.com>
2023-07-01 00:19:46 -0300

    Update app.py

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ class App(CTk):
         height_plus = (self.height_screen // 2 - self.win_height // 2)
         self.geometry(f"{self.win_width}x{self.win_height}+{width_plus}+{height_plus}")
         self.resizable(True, True)
-        self.minsize(620, 140)
+        self.minsize(620, 430)
 
         self.navbar = NavBar(self, corner_radius=0)
         self.navbar.pack(side="top", fill=X)
@@ -45,10 +45,10 @@ class App(CTk):
         self.div_listener = CTkFrame(self, fg_color=self.cget("fg_color"))
         self.div_listener.pack(side="left", fill=BOTH, expand=True, padx=2)
 
-        self.combobox_listener = CTkComboBox(self.div_listener, width=300, cursor="hand2", state="readonly", justify="center", font=("Roboto", 20))
+        self.combobox_listener = CTkComboBox(self.div_listener, width=300, cursor="hand2", state="readonly", justify="center", font=("Roboto", 20), corner_radius=5)
         self.combobox_listener.pack(pady=5)
-        self.dropdown_listener = CTkScrollableDropdown(self.combobox_listener, values=list(LANGS_TRANSLATE.keys()), width=300, frame_corner_radius=0,
-                                                       command=lambda get: change_language_listener(self, get))
+        self.dropdown_listener = CTkScrollableDropdown(self.combobox_listener, values=list(LANGS_TRANSLATE.keys()), width=300, frame_corner_radius=5,
+                                                       command=lambda get: change_language_listener(self, get), height=600, button_height=30)
         self.combobox_listener.set(DEFAULT_LISTENER)
 
         def paste_listener(event):
@@ -90,10 +90,10 @@ class App(CTk):
         self.div_translator = CTkFrame(self, fg_color=self.cget("fg_color"))
         self.div_translator.pack(side="left", fill=BOTH, expand=True, padx=2)
 
-        self.combobox_translator = CTkComboBox(self.div_translator, width=300, cursor="hand2", state="readonly", justify="center", font=("Roboto", 20))
+        self.combobox_translator = CTkComboBox(self.div_translator, width=300, cursor="hand2", state="readonly", justify="center", font=("Roboto", 20), corner_radius=5)
         self.combobox_translator.pack(pady=5)
-        self.dropdown_translator = CTkScrollableDropdown(self.combobox_translator, values=list(LANGS_TRANSLATE.keys()), width=300,
-                                                         frame_corner_radius=0, command=lambda get: change_language_translator(self, get))
+        self.dropdown_translator = CTkScrollableDropdown(self.combobox_translator, values=list(LANGS_TRANSLATE.keys()), width=300, frame_corner_radius=5,
+                                                         command=lambda get: change_language_translator(self, get), height=600, button_height=30)
         self.combobox_translator.set(DEFAULT_TRANSLATOR)
 
         self.textbox_translator = CTkTextbox(self.div_translator, state="disabled", height=450, wrap="word")
