216319c68e798223a1c9fa26b5016ee5980854bb
T. Felipe <eusouored@hotmail.com>
2023-06-30 21:03:27 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from customtkinter import *
 
+from CTkScrollableDropdown import CTkScrollableDropdown
+
 from navbar import NavBar
 
 from assets.py.load_img import ImagesLoad
 from assets.py.var import DEFAULT_LISTENER, DEFAULT_TRANSLATOR, LANGS_TRANSLATE
-from assets.py.utils.functions import change_language_listener, release_translation_translator, Thread, change_language_translator
+from assets.py.utils.functions import *
 
 from os import path
 
@@ -34,26 +36,38 @@ class App(CTk):
         width_plus = (self.width_screen // 2 - self.win_width // 2)
         height_plus = (self.height_screen // 2 - self.win_height // 2)
         self.geometry(f"{self.win_width}x{self.win_height}+{width_plus}+{height_plus}")
-        self.resizable(True, False)
-        self.minsize(520, 0)
+        self.resizable(True, True)
+        self.minsize(620, 140)
 
         self.navbar = NavBar(self, corner_radius=0)
         self.navbar.pack(side="top", fill=X)
 
-        self.div_center = CTkFrame(self, corner_radius=0, fg_color=self.cget("fg_color"))
-        self.div_center.pack(fill=BOTH, expand=True, pady=5)
-
-        self.div_listener = CTkFrame(self.div_center, fg_color=self.div_center.cget("fg_color"))
+        self.div_listener = CTkFrame(self, fg_color=self.cget("fg_color"))
         self.div_listener.pack(side="left", fill=BOTH, expand=True, padx=2)
 
-        self.lang_before_listener = DEFAULT_LISTENER
-        self.combobox_listener = CTkComboBox(self.div_listener, values=list(LANGS_TRANSLATE.keys()), width=200,
-                                             state="readonly", cursor="hand2", command=lambda get: change_language_listener(self, get))
+        self.combobox_listener = CTkComboBox(self.div_listener, width=300, cursor="hand2", state="readonly", justify="center", font=("Roboto", 20))
+        self.combobox_listener.pack(pady=5)
+        self.dropdown_listener = CTkScrollableDropdown(self.combobox_listener, values=list(LANGS_TRANSLATE.keys()), width=300, frame_corner_radius=0,
+                                                       command=lambda get: change_language_listener(self, get))
         self.combobox_listener.set(DEFAULT_LISTENER)
-        self.combobox_listener.pack()
 
         def paste_listener(event):
-            Thread(target=lambda: self.textbox_listener.after(200, lambda: release_translation_translator(self))).start()
+            def auto_detect():
+                listener = self.textbox_listener.get("0.0", "end").replace("\n", "")
+                translator = translate()
+                detect = translator.detect(listener)
+
+                for key in LANGS_TRANSLATE.keys():
+                    if detect.lang == LANGS_TRANSLATE[key]:
+                        if key == self.combobox_translator.get():
+                            self.combobox_translator.set(self.combobox_listener.get())
+
+                        self.combobox_listener.set(key)
+                        break
+
+            self.combobox_listener.after(300, Thread(target=auto_detect).start())
+            self.textbox_listener.after(350, Thread(target=lambda: release_translation_translator(self)).start())
+
             return event
 
         def key_release_listener(event):
@@ -63,30 +77,33 @@ class App(CTk):
                 self.textbox_translator.configure(state="normal")
                 Thread(target=lambda: release_translation_translator(self)).start()
 
-            if hasattr(self, 'after_id') and self.after_id is not None:
+            if self.after_id:
                 self.after_cancel(self.after_id)
-            self.after_id = self.after(300, resolve)
+            self.after_id = self.after(400, resolve)
             return event
 
         self.textbox_listener = CTkTextbox(self.div_listener, height=450, wrap="word")
         self.textbox_listener.bind("<Control-v>", paste_listener)
         self.textbox_listener.bind("<KeyRelease>", key_release_listener)
-        self.textbox_listener.pack(fill=BOTH)
+        self.textbox_listener.pack(fill=BOTH, expand=True)
 
-        self.div_translator = CTkFrame(self.div_center, fg_color=self.div_center.cget("fg_color"))
+        self.div_translator = CTkFrame(self, fg_color=self.cget("fg_color"))
         self.div_translator.pack(side="left", fill=BOTH, expand=True, padx=2)
 
-        self.lang_before_translator = DEFAULT_TRANSLATOR
-        self.combobox_translator = CTkComboBox(self.div_translator, values=list(LANGS_TRANSLATE.keys()), width=200,
-                                               state="readonly", cursor="hand2",
-                                               command=lambda get: change_language_translator(self, get))
+        self.combobox_translator = CTkComboBox(self.div_translator, width=300, cursor="hand2", state="readonly", justify="center", font=("Roboto", 20))
+        self.combobox_translator.pack(pady=5)
+        self.dropdown_translator = CTkScrollableDropdown(self.combobox_translator, values=list(LANGS_TRANSLATE.keys()), width=300,
+                                                         frame_corner_radius=0, command=lambda get: change_language_translator(self, get))
         self.combobox_translator.set(DEFAULT_TRANSLATOR)
-        self.combobox_translator.pack()
 
         self.textbox_translator = CTkTextbox(self.div_translator, state="disabled", height=450, wrap="word")
-        self.textbox_translator.pack(fill=BOTH)
+        self.textbox_translator.pack(fill=BOTH, expand=True)
 
-        self.after_idle(self.update)
+        self.protocol("WM_DELETE_WINDOW", self.exit)
+
+    def exit(self):
+        fade_out(self)
+        self.destroy()
 
 
 if __name__ == "__main__":
