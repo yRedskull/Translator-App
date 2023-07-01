000a75e0171ef1b082e69967cd565060a807a136
T. Felipe <eusouored@hotmail.com>
2023-06-30 21:03:34 -0300

    Update translate.py

--- assets/py/translate.py
+++ assets/py/translate.py
@@ -14,15 +14,16 @@ def listener_for_translator(translator):
     return source, destiny
 
 
-def translate(text, lang_translator=None):
+def translate(text=None, lang_translator=None):
     translator = Translator()
     text_translated = None
     src, dest = listener_for_translator(lang_translator)
-
-    try:
-        text_translated = translator.translate(text, src=src, dest=dest).text
-    except:
-        text_translated = None
-    finally:
-        return text_translated or text
-
+    if text:
+        try:
+            text_translated = translator.translate(text, src=src, dest=dest).text
+        except:
+            text_translated = None
+        finally:
+            return text_translated or text
+    else:
+        return translator
