1bdee4c947e68d92606712716ae3c84521e68b22
T. Felipe <eusouored@hotmail.com>
2023-06-30 21:03:36 -0300

    Update functions.py

--- assets/py/utils/functions.py
+++ assets/py/utils/functions.py
@@ -1,38 +1,51 @@
 from assets.py.translate import translate
+
 from threading import Thread
+
+from time import sleep as sl
 
 
 def change_language_listener(self, get=None):
+    listener = self.textbox_listener.get("0.0", "end").replace("\n", "")
+    translator = self.textbox_translator.get("0.0", "end").replace("\n", "")
 
     if get == self.combobox_translator.get():
-        self.combobox_translator.set(self.lang_before_listener)
-        trade_text(self)
-    else:
-        Thread(target=lambda: release_translation_listener(self)).start()
+        self.combobox_translator.set(self.combobox_listener.get())
 
-    self.lang_before_listener = get
+        if listener and translator:
+            trade_text(self, listener, translator)
+
+    if listener and translator:
+        self.combobox_listener.after(100, Thread(target=lambda: release_translation_listener(self)).start())
+
+    return self.combobox_listener.set(get)
 
 
 def change_language_translator(self, get=None):
-    if get == self.combobox_listener.get():
-        self.combobox_listener.set(self.lang_before_translator)
-        trade_text(self)
-    else:
-        Thread(target=lambda: release_translation_translator(self)).start()
-
-    self.lang_before_translator = get
-
-
-def trade_text(self):
-    self.textbox_translator.configure(state="normal")
-    translator = self.textbox_translator.get("0.0", "end").replace("\n", "")
     listener = self.textbox_listener.get("0.0", "end").replace("\n", "")
+    translator = self.textbox_translator.get("0.0", "end").replace("\n", "")
+
+    if get == self.combobox_listener.get():
+        self.combobox_listener.set(self.combobox_translator.get())
+
+        if listener and translator:
+            trade_text(self, listener, translator)
+
     if listener and translator:
-        self.textbox_translator.delete("0.0", "end")
-        self.textbox_translator.insert("0.0", listener)
-        self.textbox_translator.configure(state="disabled")
-        self.textbox_listener.delete("0.0", "end")
-        self.textbox_listener.insert("0.0", translator)
+        self.combobox_translator.after(100, Thread(target=lambda: release_translation_translator(self)).start())
+
+    return self.combobox_translator.set(get)
+
+
+def trade_text(self, listener, translator):
+    self.textbox_translator.configure(state="normal")
+    self.textbox_translator.delete("0.0", "end")
+    self.textbox_translator.insert("0.0", listener)
+    self.textbox_translator.configure(state="disabled")
+    self.textbox_listener.delete("0.0", "end")
+    self.textbox_listener.insert("0.0", translator)
+
+    return listener, translator
 
 
 def release_translation_listener(self, text=None):
@@ -49,3 +62,25 @@ def release_translation_translator(self, text=None):
     self.textbox_translator.delete("0.0", "end")
     self.textbox_translator.insert("0.0", text_translated)
     self.textbox_translator.configure(state="disabled")
+
+
+def fade_out(self):
+    for i in range(100, 0, -10):
+        if not self.winfo_exists():
+            break
+        self.attributes("-alpha", i / 100)
+        self.update()
+        sl(1 / 100)
+
+    self.attributes("-alpha", 0)
+
+
+def fade_in(self):
+    for i in range(0, 100, 10):
+        if not self.winfo_exists():
+            break
+        self.attributes("-alpha", i / 100)
+        self.update()
+        sl(1 / 100)
+
+    self.attributes("-alpha", 1)
