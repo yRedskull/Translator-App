1962fafd753143d49ce204f8c39f8dd9e3873452
yRedskull <eusouored@hotmail.com>
2023-10-10 18:24:17 -0300

    Update var.py

--- src/assets/py/var.py
+++ src/assets/py/var.py
@@ -1,37 +1,18 @@
 import os
 import json
+from deep_translator import GoogleTranslator
 
 
-LANGS_TRANSLATE = {
-                    "Português": "pt",
-
-                    "Inglês": "en",
-
-                    "Francês": "fr",
-
-                    "Chinês (Simplificado)": "zh-cn",
-
-                    "Chinês (Tradicional)": "zh-tw",
-
-                    "Ucraniano": "uk",
-
-                    "Italiano": "it",
-
-                    "Alemão": "de",
-
-                    "Espanhol": "es",
-
-                    "Russo": "ru"
-}
+LANGS_TRANSLATE = GoogleTranslator().get_supported_languages(as_dict=True)
 
 try:
-    with open(os.path.join(os.path.dirname(__file__), "assets", "json", "lang.json"), "r") as lang:
+    with open(os.path.join(os.path.dirname(os.path.dirname(__file__)), "json", "lang.json"), "r") as lang:
         loads = json.loads(lang.read())
         DEFAULT_LISTENER = loads["default_listener"]
         DEFAULT_TRANSLATOR = loads["default_translator"]
         DEFAULT_SYSTEM = loads["default_system"]
-except:
-    DEFAULT_LISTENER = "Português"
-    DEFAULT_TRANSLATOR = "Inglês"
-    DEFAULT_SYSTEM = "Português"
+except Exception as e:
+    DEFAULT_LISTENER = "english"
+    DEFAULT_TRANSLATOR = "portuguese"
+    DEFAULT_SYSTEM = "portuguese"
 
