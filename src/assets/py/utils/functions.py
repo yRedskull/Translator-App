38302a5c576012f08429e1a941a1849b25964288
yRedskull <eusouored@hotmail.com>
2023-10-10 18:24:13 -0300

    Update functions.py

--- src/assets/py/utils/functions.py
+++ src/assets/py/utils/functions.py
@@ -1,43 +1,32 @@
 from src.assets.py.translate import translate
 
-from threading import Thread
-
 from time import sleep as sl
 
 
-def change_language_listener(self, get=None):
+def select_dropdown_language(self, shooter, target, get=None):
+    if get == target.get():
+        target.set(shooter.get())
+        trade_text(self)
+
+    shooter.set(get)
+
+    if shooter == self.combobox_translator:
+        show_translation()
+
+
+def inverter_languages(self):
+    change_listener = self.combobox_translator.get()
+    change_translator = self.combobox_listener.get()
+
+    self.combobox_listener.set(change_listener)
+    self.combobox_translator.set(change_translator)
+    trade_text(self)
+
+
+def trade_text(self):
     listener = self.textbox_listener.get("0.0", "end").replace("\n", "")
     translator = self.textbox_translator.get("0.0", "end").replace("\n", "")
 
-    if get == self.combobox_translator.get():
-        self.combobox_translator.set(self.combobox_listener.get())
-
-        if listener and translator:
-            trade_text(self, listener, translator)
-
-    if listener and translator:
-        Thread(target=lambda: self.combobox_listener.after(100, release_translation_listener(self))).start()
-
-    return self.combobox_listener.set(get)
-
-
-def change_language_translator(self, get=None):
-    listener = self.textbox_listener.get("0.0", "end").replace("\n", "")
-    translator = self.textbox_translator.get("0.0", "end").replace("\n", "")
-
-    if get == self.combobox_listener.get():
-        self.combobox_listener.set(self.combobox_translator.get())
-
-        if listener and translator:
-            trade_text(self, listener, translator)
-
-    if listener and translator:
-        Thread(target=lambda: self.combobox_translator.after(100, release_translation_translator(self))).start()
-
-    return self.combobox_translator.set(get)
-
-
-def trade_text(self, listener, translator):
     self.textbox_translator.configure(state="normal")
     self.textbox_translator.delete("0.0", "end")
     self.textbox_translator.insert("0.0", listener)
@@ -48,14 +37,7 @@ def trade_text(self, listener, translator):
     return listener, translator
 
 
-def release_translation_listener(self, text=None):
-    text_translated = translate(text or self.textbox_listener.get("0.0", "end"),
-                                lang_translator=self.combobox_listener.get())
-    self.textbox_listener.delete("0.0", "end")
-    self.textbox_listener.insert("0.0", text_translated)
-
-
-def release_translation_translator(self, text=None):
+def show_translation(self, text=None):
     text_translated = translate(text or self.textbox_listener.get("0.0", "end"),
                                 lang_translator=self.combobox_translator.get())
     self.textbox_translator.configure(state="normal")
