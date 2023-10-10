a3ac19e99e91438a3834b7469e67d81927d1da81
yRedskull <eusouored@hotmail.com>
2023-10-10 18:48:12 -0300

    Update navbar.py

--- frontend/navbar.py
+++ frontend/navbar.py
@@ -26,11 +26,14 @@ class NavBar(CTkFrame):
         value_off = offvaluetheme()
         switch_theme_mode = StringVar(value=get_appearance_mode())
 
+        ghost_btn = CTkButton(self, text="", fg_color=self.cget("fg_color"), border_width=0, hover=False, width=100)
+        ghost_btn.pack(expand=True, side="left")
+
         self.btn_trade = CTkButton(self, text="⇄", font=("Arial", 30), width=50, corner_radius=40, border_width=0,
                                    command=lambda: inverter_languages(master))
         self.btn_trade.pack(expand=True, side="left")
 
         self.btn_mode = CTkSwitch(self, text=get_appearance_mode(), variable=switch_theme_mode, command=switch_theme, onvalue=get_appearance_mode(), offvalue=value_off)
-        self.btn_mode.pack(side="right", expand=True)
+        self.btn_mode.pack(side="left", expand=True)
 
 
